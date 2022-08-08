d00ed183b95bcbea0c1479ea94a8fbe8a9aaca07
RojasD13 <94007095+RojasD13@users.noreply.github.com>
2022-08-08 13:07:34 -0500

    ArrayList to Json - Json to ArrayList

--- src/controller/FileController.java
+++ src/controller/FileController.java
@@ -8,15 +8,39 @@ import java.util.Scanner;
 
 public class FileController {
     private File objectFile;
+    private File object2File;
+    private File charFile;
 
     public FileController() throws IOException {
         this.objectFile = new File("assets/object.json");
+        this.object2File = new File("assets/object2.json");
+        this.charFile = new File("assets/char.json");
     }
 
     // Para leer
     public String readObjectFile() throws FileNotFoundException {
         String text = "";
         Scanner sc = new Scanner(objectFile);
+        while (sc.hasNextLine()) {
+            text += sc.nextLine();
+        }
+        sc.close();
+        return text;
+    }
+
+    public String readObject2File() throws FileNotFoundException {
+        String text = "";
+        Scanner sc = new Scanner(object2File);
+        while (sc.hasNextLine()) {
+            text += sc.nextLine();
+        }
+        sc.close();
+        return text;
+    }
+
+    public String charFile() throws FileNotFoundException {
+        String text = "";
+        Scanner sc = new Scanner(charFile);
         while (sc.hasNextLine()) {
             text += sc.nextLine();
         }
@@ -32,9 +56,29 @@ public class FileController {
         fileWriter.close();
     }
 
+    public void writeObject2File(String text) throws IOException {
+        object2File.mkdirs();
+        FileWriter fileWriter = new FileWriter("assets/object2.json");
+        fileWriter.write(text);
+        fileWriter.close();
+    }
+
+    public void charFile(String text) throws IOException {
+        charFile.mkdirs();
+        FileWriter fileWriter = new FileWriter("assets/char.json");
+        fileWriter.write(text);
+        fileWriter.close();
+    }
+
     //Para el tamaño
     public long sizeObjectFile() {
         return objectFile.length();
+    }
+    public long sizeObject2File() {
+        return object2File.length();
+    }
+    public long sizeCharFile() {
+        return charFile.length();
     }
 
 }
--- src/controller/JsonController.java
+++ src/controller/JsonController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
 
 public class JsonController {
     private Gson gson;
@@ -9,11 +12,46 @@ public class JsonController {
         this.gson = new Gson();
     }
 
+    //ArrayList a Json
+    public String objectsToJson(ArrayList<Object> objects) {
+        return gson.toJson(objects);
+    }
+
+    public String objects2ToJson(ArrayList<Object> objects2) {
+        return gson.toJson(objects2);
+    }
+
+    public String charsToJson(ArrayList<Character> chars) {
+        return gson.toJson(chars);
+    }
+
+    //Json a ArrayList
+    public ArrayList<Object> jsonToObjects(String jsonText) {
+        ArrayList<Object> objects = gson.fromJson(jsonText, new TypeToken<ArrayList<Object>>() {
+        }.getType());
+        return objects;
+    }
+
+    public ArrayList<Object> jsonToObjects2(String jsonText) {
+        ArrayList<Object> objects2 = gson.fromJson(jsonText, new TypeToken<ArrayList<Object>>() {
+        }.getType());
+        return objects2;
+    }
+
+    public ArrayList<Character> jsonToChars(String jsonText) {
+        ArrayList<Character> chars = gson.fromJson(jsonText, new TypeToken<ArrayList<Object>>() {
+        }.getType());
+        return chars;
+    }
+
+
+    //Json a un solo object
     public Object jsonToObject(String jsonText) {
         Object object = gson.<Object>fromJson(jsonText, Object.class);
         return object;
     }
 
+    //Un solo Object a Json
     public String objectToJson(Object object) {
         return gson.toJson(object);
     }
